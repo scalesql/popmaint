b431f14bb38f15127e7c239f6e3f5661c2e42b0d
Bill Graziano <1462628+billgraziano@users.noreply.github.com>
2025-11-07 14:23:21 -0600

    don't error if we can't find the current user on Windows

--- internal/app/user.go
+++ internal/app/user.go
@@ -10,10 +10,12 @@ import (
 )
 
 // currentUserName returns the current user name in DOMAIN\USER format
+// TODO need a better solution: https://github.com/golang/go/issues/37348
 func currentUserName() (string, error) {
 	usr, err := user.Current()
 	if err != nil {
-		return "", errors.Wrap(err, "user.current")
+		return errors.Wrap(err, "user.current").Error(), nil
+		//return "", errors.Wrap(err, "user.current")
 	}
 	return usr.Username, nil
 }
